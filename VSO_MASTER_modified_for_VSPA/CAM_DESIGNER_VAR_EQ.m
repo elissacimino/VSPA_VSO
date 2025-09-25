e726e3b94966a5ce6e1189652a48582e31b3a69e
Cimino <ecimino@UMICH.EDU>
2025-09-25 13:30:44 -0400

    vspa

--- VSO_MASTER_modified_for_VSPA/CAM_DESIGNER_VAR_EQ.m
+++ VSO_MASTER_modified_for_VSPA/CAM_DESIGNER_VAR_EQ.m
@@ -173,19 +173,20 @@ for foot = {'uniform','linear zero eq', 'linear off zero'};
         thetapoints_in = [-0.65, -0.5, -0.2, 0, 0.125, 0.21, 0.24, 0.4, 0.5, 0.65];
         Mpoints_in = [-25,-25,-17,0,25,60,76,100,100,100]/70*m;
         Mpoints = interp1(thetapoints_in, Mpoints_in, deg2rad([-max_p:1:max_d]),'pchip');
+        
 
     %% These have been added to test a consistent linear TA relationship, with one having an equilbrium angle at 0 and another at -10deg
     elseif strcmp(foot, 'linear zero eq')
         thetapoints = deg2rad(-max_p:max_d);
         stiffness_linear = 15.8 * (180/pi);                  % Nm/deg to Nm/rad
         Mpoints = stiffness_linear * thetapoints;            % linear relationship from -40 to 40 degrees with stiffness of 15.8 Nm/deg
-        fileName = fullfile('inputs', 'TA_linear.mat');
-        save(fileName, 'Mpoints');
+        % fileName = fullfile('inputs', 'TA_linear.mat');
+        % save(fileName, 'Mpoints');
 
     elseif strcmp(foot, 'linear off zero')
         stiffness_linear = 15.8 * (180/pi);
-        thetapoints_offzero = deg2rad(-max_p:max_d+10);   
-        Mpoints = stiffness_linear * thetapoints_offzero;
+        thetapoints = deg2rad(-max_p:max_d+10);   
+        Mpoints = stiffness_linear * thetapoints;
         Mpoints = Mpoints(11:end);                           % horizontal shift by 10 degrees (left), making the equilibrium angle at -10deg 
         eq_index = find(Mpoints==0);                         % index where M=0, otherwise known as equilibrium angle
         equilibrium_angle = rad2deg(thetapoints(eq_index));  % convert equilibrium point from rad to deg
